3ab6d036b156c083fd63d02aeb14dab33e3ae1e4
humblEgo <injun.woo30000@gmail.com>
2020-12-29 21:21:28 +0900

    MODIFY: add comments for code separation

--- hammerspoon_config/init.lua
+++ hammerspoon_config/init.lua
@@ -1,2 +1,4 @@
+
+-- init.lua from vimlife_with_hammerspoon
 require('./modules/change_input_source_if_ko'):bind_change_input_source_with({}, "escape")
 require('./modules/vim_keymap'):bind_vim_keymap_with({"cmd", "shift"}, "space")
